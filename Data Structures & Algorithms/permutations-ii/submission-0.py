c4fa6fe1597e763a85d214edc6c1eb61314b569f
stcwy777 <uaneedcar@gmail.com>
2026-06-04 18:51:54 -0700

    Add: permutations-ii - submission-0

---
+++ Data Structures & Algorithms/permutations-ii/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        perms = [[]]
+
+        for num in nums:
+            newPerms = []
+            dedup = set()
+            for p in perms:
+                for i in range(len(p) + 1):
+                    newP = p.copy()
+                    newP.insert(i, num)
+                    if tuple(newP) not in dedup:
+                        newPerms.append(newP)
+                        dedup.add(tuple(newP))
+            perms = newPerms
+            print(perms)
+        return perms
